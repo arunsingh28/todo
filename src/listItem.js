6ba669299c9e7b4ddf3adf1555e9bce95376b1ba
arunsingh28 <arunsingh28aug.as@gmail.com>
2020-11-30 22:04:37 +0530

    v.2

--- src/listItem.js
+++ src/listItem.js
@@ -5,11 +5,9 @@ import './App.css'
 function List(props){
     const items = props.items;
     const listItems = items.map(item =>{
-        return  <div className="wrapper">
-            <div className="list-item" key="item.key">
+        return  <div className="list-item" key="item.key">
             {item.text}<i className="fa fa-trash"></i>
             </div>
-        </div>
     })
     return (
     <div>{listItems}</div>
